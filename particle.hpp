d52d55b7674f1976d91065d59610cd9aed93bd6a
Jay <jay0@me.com>
2022-01-30 23:03:01 +0000

    Add include guard

--- particle.hpp
+++ particle.hpp
@@ -1,3 +1,6 @@
+#ifndef particle_hpp // Include guard
+#define particle_hpp
+
 struct Particle {
     /* 1-dimensional values */
     double pos;
@@ -8,3 +11,5 @@ struct Particle {
 // There are no strings or other fields that require manual memory allocation
 // for Particle, so actually a factory is unneeded
 // Particle particle_factory(double pos, double vel);
+
+#endif
