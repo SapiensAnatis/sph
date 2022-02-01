6582ca1cfca69e478f13313d65baa2659fa9c310
Jay <jay0@me.com>
2022-02-01 11:07:15 +0000

    Set property values (untested)

--- src/setup.cpp
+++ src/setup.cpp
@@ -8,11 +8,12 @@ ConfigMap parse_config(std::istream &cfg_stream, int &status_code) {
     status_code = 0;
 
     std::string l;
+
     while (std::getline(cfg_stream, l)) {    
         current_line++;
 
         if (l[0] != '#' && l.length() > 0) { // Ignore comments and empty lines
-            // Split on space
+            // Split string on space
             size_t space = 0;
             std::string propname;
             std::string propvalue;
@@ -37,7 +38,7 @@ ConfigMap parse_config(std::istream &cfg_stream, int &status_code) {
             // What if the parameter is already in the map? Not a fatal error, but the user should
             // be told
             if (result_map.count(propname)) {
-                std::cout << "[WARNING] Ignoring second definition of parameter '" << propname << 
+                std::cout << "[WARN] Ignoring second definition of parameter '" << propname << 
                 "' on line " << current_line << " of config file." << std::endl; 
             }
             
@@ -47,4 +48,30 @@ ConfigMap parse_config(std::istream &cfg_stream, int &status_code) {
     }
 
     return result_map;
+}
+
+void load_properties(ConfigMap &config_map, int &status_code) {
+    // This is a bit monolithic, but I can't really think of a better way of doing it.
+    set_property(Config::n_part, "n_part", config_map["n_part"]);
+    set_property(Config::d_unit, "d_unit", config_map["d_unit"]);
+}
+
+void set_property(uint &prop, const std::string &prop_name, const std::string &prop_value) {
+    try {
+        prop = std::stoi(prop_value);
+    } catch (const std::invalid_argument& ia) {
+        std::cout << "[ERROR] Failed to parse value '" << ia.what() << "' for property '" <<
+        prop_name << "'." << std::endl;
+        exit(1);
+    }
+}
+
+void set_property(double &prop, const std::string &prop_name, const std::string &prop_value) {
+    try {
+        prop = std::stod(prop_value);
+    } catch (const std::invalid_argument& ia) {
+        std::cout << "[ERROR] Failed to parse value '" << ia.what() << "' for property '" <<
+        prop_name << "'." << std::endl;
+        exit(1);
+    }
 }
--- src/setup.hpp
+++ src/setup.hpp
@@ -1,15 +1,13 @@
 /* 
  * PHYM004 Project 2 / Jay Malhotra
  *
- * setup.hpp defines functions implemented in read_config.cpp. As you may have
- * guessed, these relate to reading the configuration file and setting up the
- * program. It also defines a static Config namespace, through which these
- * values are accessed while the program is running.
+ * setup.hpp defines functions implemented in read_config.cpp. As you may have guessed, these relate
+ * to reading the configuration file and setting up the program. It also defines a Config struct
+ * with shared static properties.
  *
- * The configuration format is proprietary, and it does reinvent the wheel
- * somewhat as there are plenty of libraries out there that do this. That said,
- * it is simple enough code that I prefer to do it this way rather than adding
- * an external dependency.
+ * The configuration format is proprietary, and it does reinvent the wheel somewhat as there are
+ * plenty of libraries out there that do this. That said, it is simple enough code that I prefer to
+ * do it this way rather than adding an external dependency.
  */
 
 #ifndef setup_hpp // Include guard
@@ -19,7 +17,7 @@
 #include <map>
 #include <string>
 
-namespace Config {
+struct Config {
     // Variable names correspond to parameter names in config file
     static double d_unit;
     static uint n_part; // Max: ~4 billion. No need for long in my opinion.
@@ -27,10 +25,21 @@ namespace Config {
 
 typedef std::map<std::string, std::string> ConfigMap; 
 
-// parse_config: takes in an infile stream of the config file, and creates a map
-// of <propertyname, propertvalue> to be properly parsed and stored later.
-// A status code is written to the second parameter -- it's 0 if everything went
-// okay, and 1 if something went wrong.
+// parse_config: takes in a stream of the config file, and creates a <string, string> map of
+// <propertyname, propertyvalue> to be properly parsed and stored by load_properties. A status code
+// is written to the second parameter -- it's 0 if everything went okay, and 1 if something went
+// wrong (e.g. parse error).
 ConfigMap parse_config(std::istream &cfg_stream, int &status_code);
+
+// load_properties: takes in a ConfigMap (from above) and initializes the values of all the needed
+// properties in the Config class. This is where datatype conversion from string will be performed.
+// It uses the same status code convention as above.
+void load_properties(ConfigMap &config_map, int &status_code);
+
+// Overloads of set_property. These take in a particular type of Config member by reference as well
+// as a string property value, and each overload has a different way of converting the property
+// value based on the type of the Config member.
+void set_property(uint &prop, const std::string &prop_name, const std::string &prop_value);
+void set_property(double &prop, const std::string &prop_name, const std::string &prop_value);
 
 #endif
