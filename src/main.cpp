b67fa1e8a67cd54e4d9c401a34aa944afc1bebe5
Jay <jay0@me.com>
2022-02-01 11:05:03 +0000

    Change line column limit/wrap

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 /* 
  * PHYM004 Project 2 / Jay Malhotra
  *
- * main.cpp contains the entrypoint for the program. All of the options are
- * given in the config file, due to my own personal trauma relating to invoking
- * sph-NG setup by piping in a 20+ line long text file without comments. As a
- * result, this entrypoint is brief and contains no user input. 
+ * main.cpp contains the entrypoint for the program. All of the options are given in the config
+ * file, due to my own personal trauma relating to invoking sph-NG setup by piping in a 20+ line
+ * long text file without comments or parameter names. As a result, this entrypoint is brief and
+ * contains no user input prompts.
  *
- * It mainly serves to look for a command line argument asking for a different
- * config file, and open whichever config file to a readable stream that is
- * given to parse_config.
- * 
- * It has no corresponding header file as there is no need for the functions
- * herein to be accessed elsewhere.
+ * It mainly serves to look for a command line argument asking for a different config file, and open
+ * whichever config file to a readable stream that is given to parse_config.
+ *
+ * It has no corresponding header file as there is no need for the functions herein to be accessed
+ * elsewhere.
  */
 
 #include <string>
@@ -37,12 +36,10 @@ int main(int argc, char* argv[]) {
     config_stream.open(filename);
 
     if (!config_stream) {
-        // The logging system is not very advanced, but hopefully using these
-        // simple tags should enable users to perform ad-hoc filtering using
-        // grep or similar
-        std::cout << "[ERROR] Could not open config file " << filename <<
-        " for reading. Perhaps the file could not be found, or you do not"
-        " have permission to read it." << std::endl;
+        // The logging system is not very advanced, but hopefully using these simple tags should
+        // enable users to perform ad-hoc filtering using grep or similar
+        std::cout << "[ERROR] Could not open config file " << filename << " for reading. Perhaps"
+        " the file could not be found, or you do not have permission to read it." << std::endl;
         
         return 1;
     }
@@ -51,8 +48,7 @@ int main(int argc, char* argv[]) {
     parse_config(config_stream, cfg_read_status);
 
     if (cfg_read_status != 0) {
-        std::cout << "[ERROR] Something went wrong while reading the config"
-        " file. Program exiting..." << std::endl;
+        std::cout << "[ERROR] Something went wrong while reading the config file. Program exiting..." << std::endl;
         return 1;
     }
 
